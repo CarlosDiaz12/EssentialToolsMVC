12faa5fdfcfb280d9ef0f9bdfdb32b7999250668
Carlos Diaz <carlos.cedp@gmail.com>
2021-10-28 15:24:34 -0400

    custom depedency resolver example

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -10,24 +10,30 @@ namespace EssentialToolsMVC.Controllers
 {
     public class HomeController : Controller
     {
+        private IValueCalculator _calc;
         private Product[] products = {
              new Product {Name = "Kayak", Category = "Watersports", Price = 275M},
              new Product {Name = "Lifejacket", Category = "Watersports", Price = 48.95M},
              new Product {Name = "Soccer ball", Category = "Soccer", Price = 19.50M},
              new Product {Name = "Corner flag", Category = "Soccer", Price = 34.95M}
          };
+
+        public HomeController(IValueCalculator calcParam)
+        {
+            _calc = calcParam;
+        }
         public ActionResult Index()
         {
-            // instance that controls all the interfaces an thrie corresponding clasess
-            IKernel ninjectKernel = new StandardKernel();
+            //// instance that controls all the interfaces an thrie corresponding clasess
+            //IKernel ninjectKernel = new StandardKernel();
 
-            // telling ninject wich interface is gointo to use what class implementation
-            ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            //// telling ninject wich interface is gointo to use what class implementation
+            //ninjectKernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
 
-            // getting the interface implementation previously registered
-            IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
+            //// getting the interface implementation previously registered
+            //IValueCalculator calc = ninjectKernel.Get<IValueCalculator>();
 
-            ShoppingCart cart = new ShoppingCart(calc)
+            ShoppingCart cart = new ShoppingCart(_calc)
             {
                 Products = products
             };
