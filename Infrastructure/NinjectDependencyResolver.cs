4f41db224a0cdc1cb2ab51bddff679c39ca43e1f
Carlos Diaz <carlos.cedp@gmail.com>
2021-10-28 15:31:14 -0400

    depdendency chaining example

--- Infrastructure/NinjectDependencyResolver.cs
+++ Infrastructure/NinjectDependencyResolver.cs
@@ -27,6 +27,7 @@ namespace EssentialToolsMVC.Infrastructure
         private void AddBindings()
         {
             kernel.Bind<IValueCalculator>().To<LinqValueCalculator>();
+            kernel.Bind<IDiscountHelper>().To<DefaultDiscountHelper>();
         }
     }
 }
